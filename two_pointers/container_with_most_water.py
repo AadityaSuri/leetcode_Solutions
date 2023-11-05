c473070f518d79968448aaec2065f7525d85e56a
Aaditya Suri <aadityasuri1@gmail.com>
2023-11-04 20:25:50 -0700

    container with most water

---
+++ two_pointers/container_with_most_water.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+
+        maxWater = 0
+        while l < r:
+            currWater = (r - l) * min(height[l], height[r])
+            maxWater = max(maxWater, currWater)
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return maxWater
