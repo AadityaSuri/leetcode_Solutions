229c9e4221e33268857ab1f5ec75a38e3394060f
Aaditya Suri <aadityasuri1@gmail.com>
2023-11-04 21:42:33 -0700

    3sum

---
+++ two_pointers/3sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+
+        seen = set()
+
+        ret = []
+        i = 0
+        while i < len(nums):
+            if nums[i] not in seen:
+                seen.add(nums[i])
+                currtarget = 0 - nums[i]
+
+                l, r = i + 1, len(nums) - 1
+                while l < r:
+                    if nums[l] + nums[r] == currtarget:
+                        ret.append([nums[i], nums[l], nums[r]])
+                        l += 1
+                        r -= 1
+                        while nums[l] == nums[l - 1] and l < r:
+                            l += 1
+                    elif nums[l] + nums[r] > currtarget:
+                        r -= 1
+                    else:
+                        l += 1
+            i += 1
+
+        return ret
